1b1dd15c8e0977b92afb8de405c564c4916b88d4
Markus J. Wetzel <markuswetzel@gmx.net>
2017-08-28 01:31:01 +0200

    Fix exports and getConnectionData

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import TransporterNetwork from './core/Network';
 import TransporterClient from './core/Client';
 import reducer from './reducers';
-import createTransporterRequest from './request/createRequest';
 import createTransporterSelector from './selector/createSelector';
 import Provider from './react/Provider';
 import withQuery from './react/withQuery';
@@ -10,7 +9,6 @@ export {
   TransporterNetwork,
   TransporterClient,
   reducer,
-  createTransporterRequest,
   createTransporterSelector,
   Provider,
   withQuery,
--- src/selector/getConnectionData.js
+++ src/selector/getConnectionData.js
@@ -1,13 +1,14 @@
 import Selector from './Selector';
 
-export default function getConnectionData(state, id, name, constraints = () => {}) {
+export default function getConnectionData(state, id, name, constraints) {
   const childrenIds =
     !state.entities[id[0]][id[1]][name] || !state.entities[id[0]][id[1]][name].connection
       ? []
       : state.entities[id[0]][id[1]][name].connection;
 
-  const selector = new Selector(state, childrenIds);
-  constraints(selector);
+  const selector = constraints
+    ? constraints(new Selector(state, childrenIds))
+    : new Selector(state, childrenIds);
 
   return selector.getData();
 }
