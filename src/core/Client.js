b9a6381355a508bfb3b1b29d94886b7ac6ce5d68
Markus J. Wetzel <markuswetzel@gmx.net>
2017-09-05 13:02:13 +0200

    Refactoring

--- src/core/Client.js
+++ src/core/Client.js
@@ -1,11 +1,12 @@
 import createRequest from '../request/createRequest';
+import reducer from '../reducers';
 
 export default class Client {
   constructor(config) {
     this.network = config.network;
   }
 
-  createMiddleware() {
+  middleware() {
     return () => next => (action) => {
       if (action.type === 'TRANSPORTER_QUERY' || action.type === 'TRANSPORTER_MUTATION') {
         return next(createRequest(action, this.network.fetch));
@@ -13,6 +14,11 @@ export default class Client {
 
       return next(action);
     };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  reducer() {
+    return reducer;
   }
 
   getConfig() {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import TransporterNetwork from './core/Network';
 import TransporterClient from './core/Client';
-import reducer from './reducers';
 import createTransporterSelector from './selector/createSelector';
 import Provider from './react/Provider';
 import withQuery from './react/withQuery';
 
-export {
-  TransporterNetwork,
-  TransporterClient,
-  reducer,
-  createTransporterSelector,
-  Provider,
-  withQuery,
-};
+export { TransporterNetwork, TransporterClient, createTransporterSelector, Provider, withQuery };
