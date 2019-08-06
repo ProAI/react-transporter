51ba22aed58a11491d52a8417d7dde2dc2389ba0
Markus J. Wetzel <markuswetzel@gmx.net>
2019-08-06 13:18:55 +0200

    Make options an optional argument

--- src/selector/select.js
+++ src/selector/select.js
@@ -1,5 +1,5 @@
 import selectAdvanced from './selectAdvanced';
 
 export default function select(ast, options) {
-  return selectAdvanced(store => store.select(ast, options));
+  return selectAdvanced(store => store.select(ast, options || {}));
 }
