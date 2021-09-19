c36c351db79fbd0b5c728e1f09f5e6e65db1d1d8
Markus J. Wetzel <markuswetzel@gmx.net>
2021-09-19 22:22:37 +0200

    Update configureStore

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -4,8 +4,6 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createRequest from '../request/createRequest';
 import createReducer from '../reducers';
 
-const development = process.env.NODE_ENV === 'development';
-
 export default function configureStore(data, network) {
   const middleware = () => (next) => (action) => {
     if (['TRANSPORTER_QUERY', 'TRANSPORTER_MUTATION'].includes(action.type)) {
@@ -24,20 +22,5 @@ export default function configureStore(data, network) {
 
   const reducer = createReducer(roots, entities);
 
-  const store = createStore(reducer, {}, composedEnhancers);
-
-  store.asyncReducers = {};
-
-  if (development && module.hot) {
-    module.hot.accept('../reducers', () => {
-      import('../reducers').then((reducerModule) => {
-        const createReducers = reducerModule.default;
-        const nextReducers = createReducers(store.asyncReducers);
-
-        store.replaceReducer(nextReducers);
-      });
-    });
-  }
-
-  return store;
+  return createStore(reducer, {}, composedEnhancers);
 }
