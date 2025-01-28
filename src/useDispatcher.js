ab6268d0d8a7b1d226b30803f57df97bd062f66c
Markus J. Wetzel <markuswetzel@gmx.net>
2025-01-28 23:49:36 +0100

    Add useState to dispatcher

--- src/useDispatcher.js
+++ src/useDispatcher.js
@@ -1,4 +1,4 @@
-import { useContext, useRef } from 'react';
+import { useContext, useState, useRef } from 'react';
 import TransporterContext from './TransporterContext';
 
 const PENDING = 'pending';
@@ -20,67 +20,64 @@ export default function useDispatcher() {
     throw new Error('Dispatcher hook is used outside of TransporterContext.');
   }
 
+  const [state, setState] = useState({
+    ok: null,
+    loading: false,
+    executed: false,
+    error: null,
+  });
+
   const instances = useRef([]);
 
-  return {
-    dispatch(ast, options) {
-      const operation = ast.definitions.find(
-        (def) => def.kind === 'OperationDefinition',
-      )?.operation;
+  const updateState = (instance) => {
+    const latestInstance = getLatestInstance(instances);
 
-      if (operation !== 'query' && operation !== 'mutation') {
-        throw new Error(
-          'No operation found. Must be either "query" or "mutation".',
-        );
-      }
+    if (latestInstance !== instance) {
+      return;
+    }
 
-      const instance =
-        operation === 'query'
-          ? context.client.query(ast, options)
-          : context.client.mutate(ast, options);
+    const { status } = instance.resource;
 
-      instances.current.push(instance);
-
-      // Return cached selector set.
-      return instance.resource.promise.then(() =>
-        instance.cache.graphData.getQuery(),
-      );
-    },
-    get ok() {
-      const instance = getLatestInstance(instances);
-
-      if (!instance || instance.status === PENDING) {
-        return null;
-      }
-
-      return instance.status === FULFILLED;
-    },
-    get loading() {
-      const instance = getLatestInstance(instances);
-
-      if (!instance) {
-        return false;
-      }
-
-      return instance.status === PENDING;
-    },
-    get executed() {
-      const instance = getLatestInstance(instances);
-
-      if (!instance) {
-        return false;
-      }
-
-      return instance.status === FULFILLED || instance.status === REJECTED;
-    },
-    get error() {
-      const instance = getLatestInstance(instances);
-
-      if (!instance || instance.status !== REJECTED) {
-        return null;
-      }
-
-      return instance.response;
-    },
+    setState({
+      ok: status === PENDING ? null : status === FULFILLED,
+      loading: status === PENDING,
+      executed: status === FULFILLED || status === REJECTED,
+      error: status !== REJECTED ? null : instance.resource.response,
+    });
   };
+
+  const dispatch = (ast, options) => {
+    const operation = ast.definitions.find(
+      (def) => def.kind === 'OperationDefinition',
+    )?.operation;
+
+    if (operation !== 'query' && operation !== 'mutation') {
+      throw new Error(
+        'No operation found. Must be either "query" or "mutation".',
+      );
+    }
+
+    const instance =
+      operation === 'query'
+        ? context.client.query(ast, options)
+        : context.client.mutate(ast, options);
+
+    instances.current.push(instance);
+
+    updateState(instance);
+
+    // Update state and return cached selector set.
+    return instance.resource.promise.then(
+      () => {
+        updateState(instance);
+        return instance.cache.graphData.getQuery();
+      },
+      (err) => {
+        updateState(instance);
+        throw err;
+      },
+    );
+  };
+
+  return [dispatch, state];
 }
